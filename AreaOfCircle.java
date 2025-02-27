ae48bc9b64e7f2130c1dbd8fc94156e9b7ac2d6d
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-02-27 11:41:07 +0530

    [Ishaan] Add: Program to caculate the area of a circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math.*;
+
+public class AreaOfCircle{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the radius of the circle: ");
+		
+		double radius = sc.nextDouble();
+		double area = Math.PI * radius*radius;
+		System.out.print("Area of the circle is " + area);
+	}
+}
