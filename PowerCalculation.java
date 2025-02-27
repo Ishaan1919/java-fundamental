dd5131a41c1f8745274a5b50252618c39b943a6e
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-02-27 12:02:13 +0530

    [Ishaan] Add: Calculating the power

---
+++ PowerCalculation.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.lang.Math.*;
+
+public class PowerCalculation{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value of number: ");
+		double number = sc.nextDouble();
+		
+		System.out.print("Enter the value of power: ");
+		double power = sc.nextDouble();
+		
+		
+		double result = Math.pow(number,power);
+		
+		System.out.print("Value of Perimeter is " + result);
+	}
+}
